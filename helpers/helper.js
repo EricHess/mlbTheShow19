aa66d57780482e80dcca885d97456f8b8f51f5da
Eric Hess <ehess84@gmail.com>
2019-04-16 12:28:37 -0700

    timer for fetch

--- helpers/helper.js
+++ helpers/helper.js
@@ -1,4 +1,11 @@
-
+/**
+ * Automated Flow to update dashboards
+ * Dashboards: Total List Table, best profit tables, bid table
+ * Total List Table: Run Fetch Players, output to that table, use local storage while it is updating
+ * Profit Range Table: Run method at end of Total List Table using default range of 250 - 10000
+ * Bid Table: Refresh every 30 seconds, utilizing method to grab fresh data, set up push alerts?
+ * 
+ */
 
 let playerList = localStorage.getItem("players") != null ? JSON.parse(localStorage.getItem("players")) : null;
 let profitList;
@@ -6,7 +13,6 @@ let profitList;
 
 
 fetchPlayers = () =>{
-    localStorage.removeItem("players");
     let loadingBar = document.querySelector(".loadingBar");
     loadingBar.innerHTML="Grabbing Players.... Please Wait."
     fetch('/requestPlayers')
@@ -16,6 +22,7 @@ fetchPlayers = () =>{
         localStorage.setItem("players", JSON.stringify(data));
         loadingBar.innerHTML="Players Loaded!"
         //Need to output on to the page in a container
+        
     })
 }
 
@@ -36,28 +43,29 @@ outbidChecker = (type, amount, name) =>{
     //NEED TO POPULATE BID TABLE
     //NEED TO CONSISTENTLY CHECK BID TABLE
     let playerElement = playerList.find(obj => obj.name == name);
-    fetch('/outbidCheck/'+playerElement.original_page)
+    fetch('/outbidCheck/'+playerElement.original_page+"?")
     .then(response => response.json())
     .then(data => {
-        let updatedCost = data.find(obj => obj.name == name);
+        let updatedInfo = data.find(obj => obj.name == name);
 
         switch(type){
             case "buyOrder":
-            updatedCost.best_buy_price == parseInt(amount) ? console.log('all good still') : console.log('outbid, new bid is: '+updatedCost.best_sell_price) 
+            updatedInfo.best_buy_price == parseInt(amount) ? console.log('all good still') : console.log('outbid, new bid is: '+updatedInfo.best_sell_price) 
             //updateBidTable with name, price, amount
-            updateBidTable();
+            updateBidTable(updatedInfo, type);
             break;
     
             case "sellOrder":
-            updatedCost.best_sell_price == parseInt(amount) ? console.log('all good still') : console.log('outbid, new bid is: '+updatedCost.best_sell_price)   
+            updatedInfo.best_sell_price == parseInt(amount) ? console.log('all good still') : console.log('outbid, new bid is: '+updatedInfo.best_sell_price)   
         }
      });
 
     
 }
 
-updateBidTable = (type, amount, name) =>{
+updateBidTable = (obj, type) =>{
     let bidTable = document.querySelector("#bidChecker .bidList");
+    console.log(obj)
     
 }
 
@@ -78,5 +86,8 @@ document.querySelector("#outbidChecker").addEventListener("submit", function(e){
     outbidChecker(orderType, amount, playerName);
 })
 
+setInterval(function() {
+    fetchPlayers();
+}, 60 * 1000);
 
 }
